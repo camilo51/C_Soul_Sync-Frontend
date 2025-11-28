498dded5f72ff983ad90db73d01798cd887d1fc3
camilo51 <pereiracamilo51@gmail.com>
2025-11-28 09:07:26 -0500

    chore: Se ajustó el layout de (main) para recibir childrens

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,7 +2,9 @@ import Emotion from "@/components/Emotion";
 import Header from "@/components/Header"
 import Navigation from "@/components/Navigation";
 import Profile from "@/components/Profile";
-export default function MainLayout() {
+export default function MainLayout({children}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <>
         <div className="flex flex-col h-screen">
@@ -19,7 +21,7 @@ export default function MainLayout() {
                 </aside>
                 <main className="w-9/12 px-2 overflow-y-auto">
                     <div className="bg-gray-950/85 w-full h-full rounded-lg">
-
+                        {children}
                     </div>
                 </main>
             </div>
